d78b73d604ebcd27b357d396c04a4025efa91ea2
Paulo Borges <pauloborgesfilho@gmail.com>
2014-01-04 20:51:30 -0300

    nrf51822: Add app_error_handler

---
+++ platform/nrf51822/nrf51822.c
@@ -0,0 +1,36 @@
+/**
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2013 Paulo B. de Oliveira Filho <pauloborgesfilho@gmail.com>
+ *  Copyright (c) 2013 Claudio Takahasi <claudio.takahasi@gmail.com>
+ *  Copyright (c) 2013 João Paulo Rechi Vita <jprvita@gmail.com>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#include <stdint.h>
+#include <string.h>
+
+#include "log.h"
+
+void app_error_handler(uint32_t error_code, uint32_t line_num,
+						const uint8_t *p_file_name)
+{
+	ERROR("NRF error 0x%04x %s:%d", error_code, p_file_name, line_num);
+}
