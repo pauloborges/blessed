4990c5e780f158632ae0de9070202e6961a852f9
Paulo Borges <pauloborgesfilho@gmail.com>
2014-01-04 20:51:27 -0300

    timer: Add timer API

---
+++ stack/timer.h
@@ -0,0 +1,34 @@
+/**
+ *  The MIT License (MIT)
+ *
+ *  Copyright (c) 2013 Paulo B. de Oliveira Filho <pauloborgesfilho@gmail.com>
+ *  Copyright (c) 2013 Claudio Takahasi <claudio.takahasi@gmail.com>
+ *  Copyright (c) 2013 João Paulo Rechi Vita <jprvita@gmail.com>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+#define TIMER_SINGLESHOT		0
+#define TIMER_REPEATED			1
+
+typedef void (*timer_cb) (void *user_data);
+
+int16_t timer_init(void);
+int16_t timer_start(uint8_t type, uint32_t ms, timer_cb cb, void *user_data);
+int16_t timer_stop(int16_t id);
