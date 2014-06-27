2b15631f04bd90f15f40883dccb7fcd42bed0a95
Paulo Borges <pauloborgesfilho@gmail.com>
2014-06-27 11:03:32 -0300

    nrf51822/timer: Implement timer API using TIMER0

--- platform/nrf51822/timer.c
+++ platform/nrf51822/timer.c
@@ -25,116 +25,202 @@
  */
 
 #include <string.h>
+#include <stdbool.h>
 
 #include <nrf51.h>
 #include <nrf51_bitfields.h>
-#include <app_timer.h>
 
 #include <blessed/errcodes.h>
 #include <blessed/timer.h>
 #include <blessed/log.h>
 
-/* nRF51 Series Reference Manual, section 18.1.2, page 101
- *
- * Prescaler for the Real Timer Counter (RTC). The prescaler defines the RTC
- * frequency. With 0, the RTC frequency assumes the maximum possible value:
- * 32768 kHz.
- */
-#define PRESCALER			0
-#define MAX_TIMERS			8
-#define OP_QUEUE_SIZE			7
+#include "nrf51822.h"
 
-#define ERROR_HANDLING(err_code)					\
-	switch (err_code) {						\
-	case NRF_ERROR_INVALID_PARAM:					\
-		return -EINVAL;						\
-	case NRF_ERROR_INVALID_STATE:					\
-		return -ENOREADY;					\
-	case NRF_ERROR_NO_MEM:						\
-		return -ENOMEM;						\
-	default:							\
-		return -EINTERN;					\
+#define HFCLK				16000000UL
+#define TIMER_PRESCALER			4		/* 1 MHz */
+#define MAX_TIMERS			4
+
+#define ROUNDED_DIV(A, B)		(((A) + ((B) / 2)) / (B))
+#define BIT(n)				(1 << n)
+
+struct timer {
+	uint32_t ticks;
+	timer_cb cb;
+	void *data;
+	uint8_t enabled:1;
+	uint8_t active:1;
+	uint8_t type:1;
+};
+
+static struct timer timers[MAX_TIMERS];
+static uint8_t active = 0;
+
+#define CLR_SET_MASKS(id, clr, set)					\
+	switch (id) {							\
+	case 0:								\
+		clr = TIMER_INTENCLR_COMPARE0_Msk;			\
+		set = TIMER_INTENSET_COMPARE0_Msk;			\
+		break;							\
+	case 1:								\
+		clr = TIMER_INTENCLR_COMPARE1_Msk;			\
+		set = TIMER_INTENSET_COMPARE1_Msk;			\
+		break;							\
+	case 2:								\
+		clr = TIMER_INTENCLR_COMPARE2_Msk;			\
+		set = TIMER_INTENSET_COMPARE2_Msk;			\
+		break;							\
+	case 3:								\
+		clr = TIMER_INTENCLR_COMPARE3_Msk;			\
+		set = TIMER_INTENSET_COMPARE3_Msk;			\
+		break;							\
 	}
 
-/* Defines a buffer where both timers and timer operations will be stored.
- * This definition comes from app_timer.h.
- */
-static uint32_t APP_TIMER_BUF[CEIL_DIV(APP_TIMER_BUF_SIZE(MAX_TIMERS,
-				OP_QUEUE_SIZE + 1), sizeof(uint32_t))];
+static __inline void update_cc(uint8_t id, uint64_t ticks)
+{
+	uint32_t clr_mask = 0;
+	uint32_t set_mask = 0;
+
+	CLR_SET_MASKS(id, clr_mask, set_mask);
+
+	NRF_TIMER0->INTENCLR = clr_mask;
+	NRF_TIMER0->TASKS_CAPTURE[id] = 1UL;
+	NRF_TIMER0->CC[id] = ((uint64_t)NRF_TIMER0->CC[id] + ticks)
+								% 0xFFFFFFFF;
+	NRF_TIMER0->INTENSET = set_mask;
+}
+
+void TIMER0_IRQHandler(void)
+{
+	uint8_t id_mask = 0;
+	uint8_t id;
+
+	for (id = 0; id < MAX_TIMERS; id++) {
+		if (NRF_TIMER0->EVENTS_COMPARE[id]) {
+			NRF_TIMER0->EVENTS_COMPARE[id] = 0UL;
+
+			if (timers[id].active)
+				id_mask |= BIT(id);
+		}
+	}
+
+	for (id = 0; id < MAX_TIMERS; id++) {
+		if (id_mask & BIT(id)) {
+			if (timers[id].type == TIMER_REPEATED) {
+				update_cc(id, (uint64_t) timers[id].ticks);
+			} else if (timers[id].type == TIMER_SINGLESHOT) {
+				timers[id].active = 0;
+			}
+
+			timers[id].cb(timers[id].data);
+		}
+	}
+}
 
 int16_t timer_init(void)
 {
-	if (!NRF_CLOCK->EVENTS_LFCLKSTARTED) {
-		NRF_CLOCK->LFCLKSRC = CLOCK_LFCLKSRC_SRC_Xtal
-						<< CLOCK_LFCLKSRC_SRC_Pos;
-		NRF_CLOCK->TASKS_LFCLKSTART = 1;
-		while (!NRF_CLOCK->EVENTS_LFCLKSTARTED);
+	if (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0UL) {
+		NRF_CLOCK->TASKS_HFCLKSTART = 1UL;
+		while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0UL);
 	}
 
-	if (app_timer_init(PRESCALER, MAX_TIMERS, OP_QUEUE_SIZE + 1,
-					APP_TIMER_BUF, NULL) != NRF_SUCCESS)
-		return -EINTERN;
+	NRF_TIMER0->MODE = TIMER_MODE_MODE_Timer;
+	NRF_TIMER0->BITMODE = TIMER_BITMODE_BITMODE_24Bit;
+	NRF_TIMER0->PRESCALER = TIMER_PRESCALER;
+
+	NRF_TIMER0->INTENCLR = TIMER_INTENCLR_COMPARE0_Msk
+						| TIMER_INTENCLR_COMPARE1_Msk
+						| TIMER_INTENCLR_COMPARE2_Msk
+						| TIMER_INTENCLR_COMPARE3_Msk;
+
+	NVIC_SetPriority(TIMER0_IRQn, IRQ_PRIORITY_HIGH);
+	NVIC_ClearPendingIRQ(TIMER0_IRQn);
+	NVIC_EnableIRQ(TIMER0_IRQn);
+
+	memset(timers, 0, sizeof(timers));
 
 	return 0;
 }
 
 int16_t timer_create(uint8_t type, timer_cb cb)
 {
-	app_timer_mode_t mode;
-	uint32_t err_code;
-	uint32_t id;
+	int16_t id;
 
 	if (cb == NULL)
 		return -EINVAL;
 
-	switch (type) {
-	case TIMER_SINGLESHOT:
-		mode = APP_TIMER_MODE_SINGLE_SHOT;
-		break;
-	case TIMER_REPEATED:
-		mode = APP_TIMER_MODE_REPEATED;
-		break;
-	default:
+	if (type != TIMER_SINGLESHOT && type != TIMER_REPEATED)
 		return -EINVAL;
+
+	for (id = 0; id < MAX_TIMERS; id++) {
+		if (!timers[id].enabled)
+			goto create;
 	}
 
-	err_code = app_timer_create(&id, mode, cb);
+	return -ENOMEM;
 
-	if (err_code == NRF_SUCCESS)
-		return id;
+create:
+	timers[id].enabled = 1;
+	timers[id].active = 0;
+	timers[id].type = type;
+	timers[id].cb = cb;
 
-	ERROR_HANDLING(err_code);
+	return id;
 }
 
 int16_t timer_start(int16_t id, uint32_t ms, void *user_data)
 {
-	uint32_t err_code;
 	uint32_t ticks;
 
 	if (id < 0)
 		return -EINVAL;
 
-	ticks = APP_TIMER_TICKS(ms, PRESCALER);
+	if (!timers[id].enabled)
+		return -EINVAL;
 
-	err_code = app_timer_start(id, ticks, user_data);
+	if (timers[id].active)
+		return -EALREADY;
 
-	if (err_code == NRF_SUCCESS)
-		return 0;
+	ticks = ROUNDED_DIV((uint64_t)ms * HFCLK,
+		1000UL * ROUNDED_DIV(2 << TIMER_PRESCALER, 2));
 
-	ERROR_HANDLING(err_code);
+	timers[id].active = 1;
+	timers[id].ticks = ticks;
+	timers[id].data = user_data;
+
+	update_cc(id, (uint64_t) ticks);
+
+	if (active == 0) {
+		NRF_TIMER0->TASKS_START = 1UL;
+	}
+
+	active++;
+
+	return 0;
 }
 
 int16_t timer_stop(int16_t id)
 {
-	uint32_t err_code;
+	uint32_t clr_mask = 0;
+	uint32_t set_mask = 0;
+
+	UNUSED(set_mask);
 
 	if (id < 0)
 		return -EINVAL;
 
-	err_code = app_timer_stop(id);
+	if (!timers[id].enabled)
+		return -EINVAL;
 
-	if (err_code == NRF_SUCCESS)
-		return 0;
+	CLR_SET_MASKS(id, clr_mask, set_mask);
+	NRF_TIMER0->INTENCLR = clr_mask;
 
-	ERROR_HANDLING(err_code);
+	timers[id].active = 0;
+	active--;
+
+	if (active == 0) {
+		NRF_TIMER0->TASKS_STOP = 1UL;
+		NRF_TIMER0->TASKS_CLEAR = 1UL;
+	}
+
+	return 0;
 }
