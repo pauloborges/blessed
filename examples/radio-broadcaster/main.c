4fa1970d1c572606c13039c592aae48a56357e4d
Paulo Borges <pauloborgesfilho@gmail.com>
2014-07-24 15:36:03 -0300

    examples/radio-broadcaster: Add static modifier to local functions

--- examples/radio-broadcaster/main.c
+++ examples/radio-broadcaster/main.c
@@ -70,7 +70,7 @@ static uint8_t idx;
 static int16_t adv_event;
 static int16_t adv_interval;
 
-void adv_interval_timeout(void *user_data)
+static void adv_interval_timeout(void *user_data)
 {
 	radio_send(channels[idx++], ADV_CHANNEL_AA, ADV_CHANNEL_CRC,
 			adv_nonconn_ind, sizeof(adv_nonconn_ind), false);
@@ -79,7 +79,7 @@ void adv_interval_timeout(void *user_data)
 		timer_start(adv_interval, ADV_INTERVAL, NULL);
 }
 
-void adv_event_timeout(void *user_data)
+static void adv_event_timeout(void *user_data)
 {
 	idx = 0;
 	adv_interval_timeout(NULL);
